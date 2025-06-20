c906abd9b55cbc3b8571c03064f743139cc8698e
Kaarti22 <kaartimondi@gmail.com>
2025-06-20 22:39:07 +0530

    add extra security to pre-approvals creation

--- backend/app/api/routes_preapproval.py
+++ backend/app/api/routes_preapproval.py
@@ -4,6 +4,7 @@ from app.core.config import SessionLocal
 from app.schemas.preapproval import PreApprovalCreate, PreApprovalOut
 from app.services.preapproval_service import PreApprovalService
 from app.dependencies.auth_dep import get_current_user
+from app.models import Visitor, Employee
 
 router = APIRouter(prefix="/preapprovals", tags=["PreApprovals"])
 
@@ -20,9 +21,19 @@ def create_preapproval(
     db: Session = Depends(get_db),
     current_user = Depends(get_current_user)
 ):
-    service = PreApprovalService(db)
     if data.employee_id != current_user["id"]:
         raise HTTPException(status_code=403, detail="Unauthorized preapproval attempt")
+    
+    visitor = db.query(Visitor).filter_by(id=data.visitor_id).first()
+    if not visitor:
+        raise HTTPException(status_code=404, detail="Visitor not found")
+    
+    employee = db.query(Employee).get(current_user["id"])
+
+    if visitor.host_employee_name.strip().lower() != employee.name.strip().lower():
+        raise HTTPException(status_code=403, detail="Only the assigned host can pre-approve this visitor.")
+
+    service = PreApprovalService(db)
     pa = service.schedule_visit(**data.dict())
     return pa
 
